ea114563cc210b393aa73a56d5b3b245659e4181
Gerald Büchler <dev@genomast.de>
2021-03-27 12:09:30 +0100

    rearrange the for-loops and have a check output

--- nscheck.sh
+++ nscheck.sh
@@ -1,40 +1,47 @@
 #!/bin/bash
 
+function main()
+{
 for x1 in {a..z}
 do
-    for x2 in {a..z}
-    do
-        for x3 in {a..z}
+        for x2 in {a..z}
         do
-            tgt=${x1}${x2}${x3}.de
-            #echo ${tgt}
-            thostq=$(host ${tgt} | grep NXDOMAIN | wc -l)
-            # echo ${thostq}
-            if [ "${thostq}" -eq "1" ]; then
-                echo .
-                echo ${tgt}
-                echo $(whois ${tgt}| grep -i "status:" -A1)
-            else
-                echo -n .
-            fi
+                #echo -n "${x1}${x2} "
+                domain=${x1}${x2}
+                check
+                for x3 in {a..z}
+                do
+                        #echo -n "${x1}${x2}${x3} "
+                        domain=${x1}${x2}${x3}
+                        check
+                done
+                for x3 in {0..9}
+                do
+                        #echo -n "${x1}${x2}${x3} "
+                        domain=${x1}${x2}${x3}
+                        check
+                done
         done
-    done
+        for x2 in {0..9}
+        do
+                #echo -n "${x1}${x2} "
+                domain=${x1}${x2}
+                check
+                for x3 in {0..9}
+                do
+                        #echo -n "${x1}${x2}${x3} "
+                        domain=${x1}${x2}${x3}
+                        check
+                done
+        done
 done
-for x1 in {a..z}
-do
-    for x2 in {00..99}
-    do
-        tgt=${x1}${x2}.de
-        #echo ${tgt}
-        thostq=$(host ${tgt} | grep NXDOMAIN | wc -l)
-        # echo ${thostq}
-        if [ "${thostq}" -eq "1" ]; then
-            echo .
-            echo ${tgt}
-            echo $(whois ${tgt}| grep -i "status:" -A1)
-        else
-            echo -n .
-        fi
-    done
-done
+echo .
 echo END
+}
+
+function check()
+{
+       echo -n "${domain}.de "
+}
+
+main
